e8a1cf52cd670e6f940d381947ed35bb0253e131
anastaciem <91271992+anastaciem@users.noreply.github.com>
2021-12-02 19:40:49 +0200

    Add files via upload

---
+++ unit6_1rec/unit6_1rec.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace unit61rec
+{
+	TEST_CLASS(unit61rec)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int i = 26;
+			int p = i % 13;
+			Assert::AreEqual(p, 0);
+		}
+	};
+}
